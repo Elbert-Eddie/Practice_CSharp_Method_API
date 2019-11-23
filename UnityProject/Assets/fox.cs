1c44b66e3192f7fd781e0fbd46a40b922b862971
Elbert-Eddie <Elbert-Eddie@users.noreply.github.com>
2019-11-23 11:24:24 +0800

    卡移動

---
+++ UnityProject/Assets/fox.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class fox : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 10;
+
+    public Rigidbody2D Rig;
+
+    public SpriteRenderer Sr;
+
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        transform.Rotate(0, speed, 0);
+    }
+}
